af56e11ad0185bd746cb43aad1ba8d2e0824bd54
AlexRamlogan02 <90638917+AlexRamlogan02@users.noreply.github.com>
2021-09-13 23:39:01 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //objective : calculate tac
+        Scanner scan = new Scanner(System.in);
+        double subtotal, total, tax = 5.5/100;
+        String state;
+        String compare = "WI";
+        NumberFormat round = NumberFormat.getCurrencyInstance();
+
+        System.out.print("What is the order amount? ");
+        subtotal = scan.nextDouble();
+        System.out.print("What is the state? ");
+        state = scan.next();
+        if (state.compareTo(compare) == 0)
+        {
+            System.out.println("The subtotal is " +round.format(subtotal));
+            tax = tax * subtotal;
+            System.out.println("The tax is " +round.format(tax));
+            total = tax + subtotal;
+            System.out.println("The total is " +round.format(total));
+            System.exit(0);
+        }
+        System.out.println("The total is " +round.format(subtotal));
+
+    }
+}
